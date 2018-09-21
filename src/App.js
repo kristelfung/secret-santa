3b1952c15514effde5c0066f8d1d08e296eaff11
Pax <kristelfung@gmail.com>
2018-09-20 22:36:57 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SecretSanta from './SecretSanta'
+
+ReactDOM.render(<SecretSanta />, document.getElementById('root'));
---
+++ src/SecretSanta.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SecretSanta extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React</h1>
+      </div>
+    );
+  }
+}
+
+export default SecretSanta;

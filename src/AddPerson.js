65baed18a3d936c19cc5bd7f951b9cecb434bdff
Kristel Fung <kristelfung@gmail.com>
2018-10-05 13:48:48 -0400

    padding to input field

--- src/AddPerson.js
+++ src/AddPerson.js
@@ -12,7 +12,7 @@ class AddPerson extends Component {
         return (
             <div className="form-wrap">
                 <form onSubmit={this.handleAddPersonChild} id="name-form" className="form">
-                    <input type="text" name="name" className="input"/>
+                    <input type="text" name="name" className="input" placeholder="Enter a name..."/>
                     <button type="submit" className="add">Add</button>
                 </form> 
             </div>

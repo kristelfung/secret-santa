2c37bc6dd77cb9176fcd430ca2b73f17c80d9d22
Kristel Fung <kristelfung@gmail.com>
2018-10-13 07:57:53 -0400

    make person stateless component

--- src/Person.js
+++ src/Person.js
@@ -1,29 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Person extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="person">
-                <div className="person__number">
-                    {this.props.number}
-                </div>
-                <p className="person__name">
-                    {this.props.person}
-                </p>
-                {
-                    (this.props.renderRemove === true)
-                    ? <button className="person__remove" onClick={(e) => {this.props.handleDeletePerson(this.props.person)}}>
-                        Remove
-                    </button>
-                    : <a href="" onClick={this.copyText} className="person__link">Copy link</a>
-                }
+const Person = (props) => {
+    return (
+        <div className="person">
+            <div className="person__number">
+                {props.number}
             </div>
-        )
-    }
+            <p className="person__name">
+                {props.person}
+            </p>
+            {
+                (props.renderRemove === true)
+                ? <button className="person__remove" onClick={(e) => {props.handleDeletePerson(props.person)}}>
+                    Remove
+                </button>
+                : <a href={ "/?name=" + props.person + "&key=" + props.encryptString(props.santa)} className="person__link">Copy link</a>
+            }
+        </div>
+    )
 }
 
 export default Person;

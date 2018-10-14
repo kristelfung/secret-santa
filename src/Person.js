e7ef15bc75eafbecccff0064c6521d03d377ffcf
Kristel Fung <kristelfung@gmail.com>
2018-10-13 20:23:28 -0400

    change favicon and make Person component not stateless

--- src/Person.js
+++ src/Person.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const Person = (props) => {
-    return (
-        <div className="person">
-            <div className="person__number">
-                {props.number}
+class Person extends Component {
+    render() {
+        return (
+            <div className="person">
+                <div className="person__number">
+                    {this.props.number}
+                </div>
+                <p className="person__name">
+                    {this.props.person}
+                </p>
+                {
+                    (this.props.renderRemove === true)
+                    ? <button className="person__remove" onClick={(e) => {this.props.handleDeletePerson(this.props.person)}}>
+                        Remove
+                    </button>
+                    : <a href={ "/?name=" + this.props.person + "&key=" + this.props.encryptString(this.props.santa)} className="person__link">Copy link</a>
+                }
             </div>
-            <p className="person__name">
-                {props.person}
-            </p>
-            {
-                (props.renderRemove === true)
-                ? <button className="person__remove" onClick={(e) => {props.handleDeletePerson(props.person)}}>
-                    Remove
-                </button>
-                : <a href={ "/?name=" + props.person + "&key=" + props.encryptString(props.santa)} className="person__link">Copy link</a>
-            }
-        </div>
-    )
+        )
+    }
 }
 
 export default Person;

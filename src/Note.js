14df08d11ea719350a996a5c4d86bf8f1d795887
Kristel Fung <kristelfung@gmail.com>
2018-12-02 21:27:18 -0500

    fix %20 bug

--- src/Note.js
+++ src/Note.js
@@ -5,7 +5,7 @@ const Note = (props) => {
     return (
         <div className="people-container">
             <div className="note">
-                <h5 className="note__title">Hi {props.name},</h5>
+                <h5 className="note__title">Hi {props.name.replace("%20", " ")},</h5>
                 <h5>you've been assigned <span className="note__santa">{props.decryptHash(props.mykey)}</span>.</h5>
                 <img src={present} className="note__image" alt="present"/>
                 <h5>Good luck!</h5>

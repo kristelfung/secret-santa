3bff45d3fb26fefddc534035d2b9c719240a7c62
Kristel Fung <kristelfung@gmail.com>
2019-07-09 15:20:30 -0400

    add space between <3 and by

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer text-center">
-            <p>Made with <span role="img" aria-label="heart">❤️</span>by <a href="https://kristelfung.com/">Kristel Fung</a>.</p>
+            <p>Made with <span role="img" aria-label="heart">❤️</span> by <a href="https://kristelfung.com/">Kristel Fung</a>.</p>
         </footer>
     )
 }
